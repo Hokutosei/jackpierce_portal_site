248e6760c4a2c9f4dd1d627b2135145f82597a78
jeanepaul <jeanepaul@gmail.com>
2013-01-16 11:40:26 +0900

    rss url fix

--- wp-content/themes/twentytwelve/rss-page.php
+++ wp-content/themes/twentytwelve/rss-page.php
@@ -28,7 +28,7 @@
 
             // Get a SimplePie feed object from the specified feed source.
             $site_url = get_site_url();
-            $rss = fetch_feed("$site_url/\wordpress/\feed/");
+            $rss = fetch_feed("$site_url/\wordpress/\feed-rss2/");
             if (!is_wp_error( $rss ) ) : // Checks that the object is created correctly
                 // Figure out how many total items there are, but limit it to 5.
                 $maxitems = $rss->get_item_quantity(20);
